3580d1d9471430a90826d1d9b9a173ecbcd456f2
gorhkd <gorhkd223@gmail.com>
2025-02-27 10:31:52 +0900

    [Bronze V] Title: 구구단, Time: 184 ms, Memory: 18412 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cmd = scanner.nextInt();
+
+        System.out.println(cmd + " * 1 = " + cmd);
+        System.out.println(cmd + " * 2 = " + cmd * 2);
+        System.out.println(cmd + " * 3 = " + cmd * 3);
+        System.out.println(cmd + " * 4 = " + cmd * 4);
+        System.out.println(cmd + " * 5 = " + cmd * 5);
+        System.out.println(cmd + " * 6 = " + cmd * 6);
+        System.out.println(cmd + " * 7 = " + cmd * 7);
+        System.out.println(cmd + " * 8 = " + cmd * 8);
+        System.out.println(cmd + " * 9 = " + cmd * 9);
+
+        }
+    }
