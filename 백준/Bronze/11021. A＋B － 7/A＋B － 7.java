fe9b6e47255fe5bd1c97ea26bf4892c4bf518f13
gorhkd <gorhkd223@gmail.com>
2025-02-28 10:19:13 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 16068 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < count + 1; i++) {
+            String[] num = br.readLine().split(" ");
+            int sum = Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+            bw.write("Case #" + i  + ": " + sum + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
