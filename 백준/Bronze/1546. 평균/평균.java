fe2da37950185fa3f2e94f9d450087e9f266178d
gorhkd <gorhkd223@gmail.com>
2025-10-07 21:36:24 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 15276 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Double M = 0.0;
+        Double[] arr = new Double[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Double.parseDouble((st.nextToken()));
+            if (M < arr[i]) M = arr[i];
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = (arr[i] * 100) / M ;
+        }
+
+        Double result = 0.0;
+        for (int i = 0; i < N; i++) {
+            result += arr[i];
+        }
+
+        System.out.println(result / N);
+    }
+}
