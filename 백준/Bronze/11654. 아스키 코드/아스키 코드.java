a50f744d3e70e0d7121990093591903ad85e2b36
gorhkd <gorhkd223@gmail.com>
2025-10-08 21:00:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        byte b = (byte)br.readLine().charAt(0);
+        System.out.println(b);
+    }
+}
