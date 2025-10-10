1c5b408479f32d5dc9b8b02adec9b26984a19b44
gorhkd <gorhkd223@gmail.com>
2025-10-11 02:54:27 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -4,23 +4,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[][] arr = new String[5][15];
+        char[][] arr = new char[5][15];
 
         for (int i = 0; i < 5; i++) {
             String c = br.readLine();
-            for (int j = 0; j < 15; j++) {
-                if (c.length() <= j) {
-                    arr[i][j] = "?";
-                } else {
-                    arr[i][j] = String.valueOf(c.charAt(j));
-                }
+            for (int j = 0; j < c.length(); j++) {
+                arr[i][j] = c.charAt(j);
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
-                if (arr[j][i] != "?") {
+                if (arr[j][i] != '\0') {
                     sb.append(arr[j][i]);
                 }
             }
