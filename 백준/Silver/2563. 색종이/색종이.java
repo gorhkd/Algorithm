71ccc7b7ad8504f393b9d3c3c2221c012f207e14
gorhkd <gorhkd223@gmail.com>
2025-10-11 16:44:36 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        boolean[][] num = new boolean[100][100];
+
+        for (int i = 0; i < cnt; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int low = Integer.parseInt(st.nextToken());
+            int col = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    num[low + j][col + k] = true;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int j = 0; j < 100; j++) {
+            for (int k = 0; k < 100; k++) {
+                if (num[j][k] == true) {
+                    sum ++;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
