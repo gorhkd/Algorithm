1dfd66553882218a00d386e5beb7e4b85727232c
gorhkd <gorhkd223@gmail.com>
2025-10-28 18:26:35 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1008 ms, Memory: 172772 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -6,18 +6,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int ownedCount = Integer.parseInt(br.readLine());
-        HashMap<Integer, Integer> ownedMap = new HashMap<>();
+        HashMap<Integer, Integer> ownedMap = new HashMap<>(ownedCount * 2);
         StringTokenizer st1 = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < ownedCount; i++) {
             int n = Integer.parseInt(st1.nextToken());
 
-            if (!ownedMap.containsKey(n)) {
-                ownedMap.put(n, 1);
-            } else {
-                int value = ownedMap.get(n) + 1;
-                ownedMap.put(n, value);
-            }
+            ownedMap.put(n, ownedMap.getOrDefault(n, 0) + 1);
         }
 
         int checkCount = Integer.parseInt(br.readLine());
@@ -27,11 +22,9 @@ public class Main {
         for (int i = 0; i < checkCount; i++) {
             int n = Integer.parseInt(st2.nextToken());
 
-            if (ownedMap.containsKey(n)) {
-                sb.append(ownedMap.get(n)).append(" ");
-            } else {
-                sb.append(0).append(" ");
-            }
+            sb.append(ownedMap.getOrDefault(n, 0));
+
+            if (i < checkCount - 1) sb.append(" ");
         }
 
         System.out.println(sb);
