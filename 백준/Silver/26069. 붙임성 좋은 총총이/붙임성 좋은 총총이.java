703798a260787a60781b30c172fec2a14924f18c
gorhkd <gorhkd223@gmail.com>
2025-12-12 14:28:35 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 120 ms, Memory: 14752 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set =  new HashSet<>();
+        set.add("ChongChong");
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String A = st.nextToken();
+            String B = st.nextToken();
+
+            if (set.contains(A)) set.add(B);
+            if (set.contains(B)) set.add(A);
+        }
+
+        System.out.println(set.size());
+    }
+}
