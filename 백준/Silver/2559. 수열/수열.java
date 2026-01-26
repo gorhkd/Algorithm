ab2708c7123bd0885d257d4f22948514dac48a19
gorhkd <gorhkd223@gmail.com>
2026-01-26 17:19:49 +0900

    [Silver III] Title: 수열, Time: 244 ms, Memory: 24044 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        arr[1] = Integer.parseInt(st.nextToken());
+
+        for (int i = 2; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        int ans = Integer.MIN_VALUE;
+
+        for (int i = K; i <= N; i++) {
+            ans = Math.max(ans, arr[i] - arr[i - K]);
+        }
+
+        System.out.println(ans);
+    }
+}
