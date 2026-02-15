5be06d966317ea00faadf7dd938731f811b5ad13
gorhkd <gorhkd223@gmail.com>
2026-02-15 13:07:37 +0900

    [Gold IV] Title: 체스판 다시 칠하기 2, Time: 348 ms, Memory: 39788 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.java
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.java
@@ -1,53 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[][] startB = new int[N + 1][M + 1];
-        int[][] startW = new int[N + 1][M + 1];
+
+        // ps[i][j] = (1,1)이 'B'로 시작하는 체스판 패턴 기준 mismatch(0/1)의 2D prefix sum
+        int[][] ps = new int[N + 1][M + 1];
 
         for (int i = 1; i <= N; i++) {
-            String s = br.readLine();
-
+            String row = br.readLine();
             for (int j = 1; j <= M; j++) {
-                char c = s.charAt(j - 1);
+                char c = row.charAt(j - 1);
 
-                if (i % 2 == 1) {
-                    if ((j % 2 == 1 && c == 'W') || j % 2 == 0 && c == 'B') {
-                        startB[i][j] = startB[i][j - 1] + startB[i - 1][j] + 1 - startB[i - 1][j - 1];
-                        startW[i][j] = startW[i][j - 1] + startW[i - 1][j] - startW[i - 1][j - 1];
-                    } else {
-                        startB[i][j] = startB[i][j - 1] + startB[i - 1][j] - startB[i - 1][j - 1];
-                        startW[i][j] = startW[i][j - 1] + startW[i - 1][j] + 1 - startW[i - 1][j - 1];
-                    }
-                } else {
-                    if ((j % 2 == 1 && c == 'B') || j % 2 == 0 && c == 'W') {
-                        startB[i][j] = startB[i][j - 1] + startB[i - 1][j] + 1 - startB[i - 1][j - 1];
-                        startW[i][j] = startW[i][j - 1] + startW[i - 1][j] - startW[i - 1][j - 1];
-                    } else {
-                        startB[i][j] = startB[i][j - 1] + startB[i - 1][j] - startB[i - 1][j - 1];
-                        startW[i][j] = startW[i][j - 1] + startW[i - 1][j] + 1 - startW[i - 1][j - 1];
-                    }
-                }
+                // (i+j) 짝수면 expected='B', 홀수면 expected='W' (좌상단 B 시작 가정)
+                char expected = (((i + j) & 1) == 0) ? 'B' : 'W';
+                int mismatch = (c == expected) ? 0 : 1;
+
+                ps[i][j] = ps[i - 1][j] + ps[i][j - 1] - ps[i - 1][j - 1] + mismatch;
             }
         }
 
         int ans = Integer.MAX_VALUE;
+        int kk = K * K;
 
-        for (int i = N; i >= K; i--) {
-            for (int j = M; j >= K; j--) {
-                int b = startB[i][j] - startB[i - K][j] - startB[i][j - K] + startB[i - K][j - K];
-                int w = startW[i][j] - startW[i - K][j] - startW[i][j - K] + startW[i - K][j - K];
-                ans = Math.min(ans, Math.min(b, w));
+        // (i, j)를 KxK 구간의 오른쪽-아래 꼭짓점으로 두고 구간합 계산
+        for (int i = K; i <= N; i++) {
+            for (int j = K; j <= M; j++) {
+                int x = ps[i][j] - ps[i - K][j] - ps[i][j - K] + ps[i - K][j - K]; // start='B' repaint
+                int repaint = Math.min(x, kk - x); // start='W'는 보완
+                if (repaint < ans) ans = repaint;
             }
         }
+
         System.out.println(ans);
     }
 }
