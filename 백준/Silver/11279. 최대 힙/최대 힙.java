07aed5a0a53d3d09ba06bd993f8bf25335d83e3f
gorhkd <gorhkd223@gmail.com>
2026-02-24 14:56:23 +0900

    [Silver II] Title: 최대 힙, Time: 292 ms, Memory: 27164 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int next = Integer.parseInt(br.readLine());
+
+            if (next == 0) {
+                if (queue.isEmpty()) {
+                    sb.append(0);
+                } else {
+                    sb.append(queue.poll());
+                }
+
+                sb.append('\n');
+            } else {
+                queue.add(next);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
